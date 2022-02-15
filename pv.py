f6060e0152e5296a107e91acd56a4881d87ab478
pietrolubello <pietrolubello@gmail.com>
2022-02-15 11:05:35 +0100

    TMY option added to PV

--- pv.py
+++ pv.py
@@ -9,11 +9,30 @@ from temp_functions import cache_func
 # Geographic location - Liège
 # Weather -  TMY (2006-2016)
 coordinates = (50.6,5.6,'Europe/Brussels',60,'Etc/GMT-2')
+surface_tilt = 38
 
 
-def pvlib_detailed(coordinates):
+def pvlib_detailed(coordinates,surface_tilt):
+    
     """
     PV production as taken from pvlib example
+    
+    Parameters
+    ----------
+    coordinates : tuple
+        latitude, longitude, name, altitude, timezone
+    surface_tilt : float
+        PV panels surface tilt [degrees]
+
+    Returns
+    -------
+    ac_15min : series
+        pandas 15min timeseries AC power production [kW/kWp]
+    losses : float
+        average inverter losses [-]
+    dc_peak['p_mp']: float
+        DC peak power production from paanel [W]
+
     """
     
     latitude, longitude, name, altitude, timezone = coordinates
@@ -27,8 +46,9 @@ def pvlib_detailed(coordinates):
     inverter = sapm_inverters['ABB__MICRO_0_25_I_OUTD_US_208__208V_']
     temperature_model_parameters = pvlib.temperature.TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
     # Defining the system
+    # NB here azimuth 0 = north 180 = south
     system = {'module': module, 'inverter': inverter,'surface_azimuth': 180}   
-    system['surface_tilt'] = latitude
+    system['surface_tilt'] = surface_tilt
     # Calculating production
     solpos = pvlib.solarposition.get_solarposition(
         time=weather.index,
@@ -106,20 +126,36 @@ def pvlib_detailed(coordinates):
     index60min = pd.date_range(start='2015-01-01 00:00:00',end='2015-12-31 23:00:00',freq='60T')
     index15min = pd.date_range(start='2015-01-01 00:00:00',end='2015-12-31 23:45:00',freq='15T')
     ac_60min = pd.Series(data=ac_np,index=index60min)
-    ac_15min = ac_60min.resample('15T').pad().reindex(index15min,method='nearest')
-    
+    ac_15min = ac_60min.reindex(ac_60min.index.union(index15min)).interpolate(method='time').reindex(index15min)    
     
     # Adimensionalized wrt peak power
-    ac_15min = ac_15min/ac_peak # W/Wp
+    ac_15min = ac_15min/dc_peak['p_mp'] # W/Wp
     
-    return ac_15min, losses
+    return ac_15min,losses,dc_peak['p_mp']
 
 @cache_func
 def pvgis_hist(inputs):
     """
     PV production taken from PVGIS data
+
+    Parameters
+    ----------
+    inputs : dictionary
+        'location': tuple latitude,longitude,name,altitude,timezone
+        'Ppeak': float peak DC power [kWp]
+        'year': int year used for output data and to get data from PVGIS if TMY = False
+        'losses': float losses in cables, power inverters, dirt (sometimes snow), over the years loss of power [%]
+        'tilt':  float surface tilt [deg]
+        'azimuth': float azimuth angle 0 = south, 180 = north [deg]
+        'TMY': bool true if data of TMY is to be used
+
+    Returns
+    -------
+    pv_15min : series
+        pandas 15 min timeseries of AC power production [kW/kWp]
+
     """
-    # Geographic location
+
 
     latitude,longitude,name,altitude,timezone = inputs['location']
     peakp = inputs['Ppeak']
@@ -127,20 +163,30 @@ def pvgis_hist(inputs):
     losses = inputs['losses']
     tilt = inputs['tilt']
     azimuth = inputs['azimuth']
+    tmybool = inputs['TMY']
     
     index60min = pd.date_range(start=str(year)+'-01-01 00:00:00',end=str(year)+'-12-31 23:00:00',freq='60T')
     index15min = pd.date_range(start=str(year)+'-01-01 00:00:00',end=str(year)+'-12-31 23:45:00',freq='15T')
     
+    if tmybool:
+        weather = pvlib.iotools.get_pvgis_tmy(latitude, longitude, map_variables=True)[0]
+        refindex = weather.index
+        refindex = refindex.shift(10,'T')
+    else:
+        refindex = pd.date_range(start=str(year)+'-01-01 00:00:00',end=str(year)+'-12-31 23:00:00',freq='60T',tz='utc')
+        refindex = refindex.shift(10,'T')
+    
     # Actual production calculation (extract all available data points)
+    # NB here azimuth 0 = south 180 = north
     res = pvlib.iotools.get_pvgis_hourly(latitude,longitude,surface_tilt=tilt,surface_azimuth=azimuth,pvcalculation=True,peakpower=peakp,loss=losses)
     
     # Index to select TMY relevant data points
     pv = res[0]['P']
-    pv = pv[pv.index.year==year]
+    pv = pv[refindex]
     pv.index = index60min
     
     # Resampling at 15 min
-    pv_15min = pv.reindex(pv.index.union(index15min)).interpolate(method='time').reindex(index15min)
+    pv_15min = pv.reindex(pv.index.union(index15min)).interpolate(method='time').reindex(index15min)/1000./peakp
     
     return pv_15min
 
@@ -148,14 +194,22 @@ def pvgis_hist(inputs):
 
 
 if __name__ == "__main__":
+    
     """
     Testing differences
     """
     
-    ac_15min, losses = pvlib_detailed(coordinates)
+    ac_15min, losses, dc_peak = pvlib_detailed(coordinates,surface_tilt)
     
-    inputs = {'location':coordinates,'Ppeak':ac_15min.max()/1000,'losses':losses,'tilt':coordinates[0],'azimuth':0,'year':2015}
-    pv_15min = pvgis_hist(inputs)
+    inp_test = {'location':coordinates,
+               'Ppeak': dc_peak/1000.,
+               'losses':losses,
+               'tilt': surface_tilt,
+               'azimuth':0,
+               'year':2015,
+               'TMY': True}
+    
+    pv_15min = pvgis_hist(inp_test)
     
     sum_pvlib = np.sum(ac_15min)/4
     print('Annual production with pvlib example system: {:.2f} kWh/kWp'.format(sum_pvlib))
@@ -168,18 +222,28 @@ if __name__ == "__main__":
     diff_y_abs = np.sum(abs(pv_15min-ac_15min))/4.
     print("Sum of absolute value of differences throughout whole year: {:.2f} kWh/kWp".format(diff_y_abs))
     
-    
     """
-    Saving PV data
+    PV data to be saved
     """
     
-    path = r'./simulations'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    filename = 'pv.pkl'
-    filename = os.path.join(path,filename)
-    # ac_15min.to_pickle(filename)
-    pv_15min.to_pickle(filename)
+    inp_save = {'location':coordinates,
+                'Ppeak': 1.0,
+                'losses': 14.,
+                'tilt':surface_tilt,
+                'azimuth':0,
+                'year':2015,
+                'TMY': True}
+
+    pv_15min2 = pvgis_hist(inp_save)   
+    sum_pvgis2 = np.sum(pv_15min2)/4
+    print(sum_pvgis2)
+    
+    # path = r'./simulations'
+    # if not os.path.exists(path):
+    #     os.makedirs(path)
+    # filename = 'pv.pkl'
+    # filename = os.path.join(path,filename)
+    # pv_15min2.to_pickle(filename)
 
 
 
