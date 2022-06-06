8cd67baacc03bca24da7b80c05d9d64c18aa94f7
pielube <pietrolubello@gmail.com>
2022-06-06 11:01:28 +0200

    test functions for heating and HP shifting

--- test_househeating.py
+++ test_househeating.py
@@ -12,9 +12,13 @@ import defaults
 index1min  = pd.date_range(start='2015-01-01',end='2015-12-31 23:59:00',freq='T')
 index10min = pd.date_range(start='2015-01-01',end='2015-12-31 23:59:00',freq='10T')
 index15min = pd.date_range(start='2015-01-01',end='2015-12-31 23:59:00',freq='15T')
-n1min = len(index1min)
+index60min = pd.date_range(start='2015-01-01',end='2015-12-31 23:59:00',freq='60T')
+n1min  = len(index1min)
+n60min = len(index60min)
 stepperh_1min = 60
 stepperh_15min = 4
+ts = 1/60
+
 
 """
 Demand of always the same house
@@ -23,20 +27,24 @@ Demand of always the same house
 temp, irr = load_climate_data()
 conf = load_config('case16')
 config,pvbatt_param,econ_param,tariffs,housetype,N = conf['config'],conf['pvbatt_param'],conf['econ_param'],conf['tariffs'],conf['housetype'],conf['N']
+
+# housetype['HP']['HeatPumpThermalPower'] = 6000.
+
 # buildtype='Freestanding'
 # wellinsulated = True
 # procebinp = ProcebarExtractor(buildtype,wellinsulated)
-procebinp={ 'ACH_infl': 0.6,        # forcing thermal parameters
-  'ACH_vent': 0.6,
-  'Afloor': 42.56,
-  'Aglazed': 16.43,
-  'Aopaque': 105.07360152315982,
-  'Atotal': 262.7636015231598,
-  'Ctot': 300000.,#157148.60871502483,
-  'Uwalls': 0.32,
-  'Uwindows': 2,
+
+procebinp={ 'ACH_infl': 1.,        #forcing thermal parameters
+  'ACH_vent': 0.0,
+  'Afloor': 112.0,
+  'Aglazed': 16.4,
+  'Aopaque': 68.6,
+  'Atotal': 164.1,
+  'Ctot': 160000.,
+  'Uwalls': 5.0,
+  'Uwindows': 5.0,
   'VentEff': 0.0,
-  'volume': 224.99799999999996}
+  'volume': 225.0}
 members = ['FTE','FTE','U12']       # forcing members
 out = compute_demand(housetype,N,members= members,thermal_parameters=procebinp)
 occ = out['occupancy'][0]
@@ -49,28 +57,34 @@ occupancy_15min = occupancy_10min.reindex(index15min,method='nearest')
 Recompute thermal demand
 """
 
-
-procebinp={ 'ACH_infl': 0.6,
-  'ACH_vent': 0.6,
-  'Afloor': 42.56,
-  'Aglazed': 16.43,
-  'Aopaque': 105.07360152315982,
-  'Atotal': 262.7636015231598,
-  'Ctot': 300000.,#157148.60871502483,
-  'Uwalls': 0.1,#0.32,
-  'Uwindows': 0.5,#2,
+procebinp={ 'ACH_infl': 1.,        #forcing thermal parameters
+  'ACH_vent': 0.0,
+  'Afloor': 112.0,
+  'Aglazed': 16.4,
+  'Aopaque': 68.6,
+  'Atotal': 164.1,
+  'Ctot': 160000.,
+  'Uwalls': 5.0,
+  'Uwindows': 5.0,
   'VentEff': 0.0,
-  'volume': 224.99799999999996}
+  'volume': 225.0}
+
 housetype['HP'] = {**housetype['HP'],**procebinp}
 
 
 Tset_ref = np.full(n1min,defaults.T_sp_low) + np.full(n1min,defaults.T_sp_occ-defaults.T_sp_low) * occupancy_1min
 
-fracmaxP = defaults.fracmaxP 
+from functions import HPSizing
+housetype['HP']['HeatPumpThermalPower'] = None
+fracmaxP = 1.0 #defaults.fracmaxP 
 QheatHP = HPSizing(housetype,fracmaxP)
 
+# QheatHP = 8000.
+# housetype['HP']['HeatPumpThermalPower'] = QheatHP
+
+
 Qintgains = out['results'][0]['InternalGains']    
-Qheat,Tin_heat = HouseHeating(housetype,QheatHP,Tset_ref,Qintgains,temp,irr,n1min,defaults.heatseas_st,defaults.heatseas_end)
+Qheat,Tin_heat = HouseHeating(housetype,QheatHP,Tset_ref,Qintgains,temp,irr,n1min,defaults.heatseas_st,defaults.heatseas_end,ts)
 
 Eheat = np.zeros(n1min+1)
 for i in range(n1min):
@@ -109,7 +123,7 @@ time1 = time.time()
 """ Tariff shifting strategy """
 # Shifting-specific inputs
 Tincrease = 3. # °C T increase wrt min T setpoint (heating off)
-t_preheat = 3  # h max time allowed to consider pre-heating
+t_preheat = 1  # h max time allowed to consider pre-heating
 t_preheat_min = t_preheat*60
 
 Tset_ref = np.full(n1min,defaults.T_sp_low) + np.full(n1min,defaults.T_sp_occ-defaults.T_sp_low) * occupancy_1min
@@ -137,7 +151,7 @@ idx = np.where(admprices*admhours)[0]
 Tset += offset
 Tset[idx] += Tincrease
 
-Qheat_sh,Tin_sh = HouseHeating(housetype,QheatHP,Tset,Qintgains,temp,irr,n1min,heatseas_st,heatseas_end)
+Qheat_sh,Tin_sh = HouseHeating(housetype,QheatHP,Tset,Qintgains,temp,irr,n1min,heatseas_st,heatseas_end,ts)
 
 time2 = time.time()
 print('It took {:.2f} seconds'.format(time2 - time1))
@@ -189,6 +203,11 @@ yprices_15min = yenprices_15min + ygridfees_15min  # €/kWh
 xx1 = sum(demand_HP*yprices_15min)
 xx2 = sum(demand_HP_shift*yprices_15min)
 
+xxx = demand_HP_shift - demand_HP
+yy1 = abs(sum(xxx[np.where(xxx>0)[0]]))
+yy2 = abs(sum(xxx[np.where(xxx<0)[0]]))
+
+
 """
 Plots
 """
@@ -199,23 +218,61 @@ ax1 = ax.twinx()
 
 demand_HP_shift.loc[day].plot(ax=ax,legend=True)
 
-occgraph = occupancy_15min*3
-occgraph.loc[day].plot.area(ax=ax,alpha=0.3)
+
+df = occupancy_15min.loc[day] > 0
+fst = df.index[df & ~ df.shift(1).fillna(False)]
+lst = df.index[df & ~ df.shift(-1).fillna(False)]
+pr = [(i, j) for i, j in zip(fst, lst)]
+for i, j in pr:
+    ax.axvspan(i,j, color='tab:green', alpha=0.3)
+
 
 # Tset_ref.loc[day].plot(ax=ax1,c='tab:green',legend=True)
 # Tset.loc[day].plot(ax=ax1,c='tab:red',legend=True)
-Tin_pre.loc[day].plot(ax=ax1,c='tab:green',ls='dashed',legend=True)
-Tin_post.loc[day].plot(ax=ax1,c='tab:red',ls='dashed',legend=True)
+Tin_pre.loc[day].plot(ax=ax1,c='tab:blue',ls='dashed',legend=True)
+Tin_post.loc[day].plot(ax=ax1,c='tab:orange',ls='dashed',legend=True)
+
+ax1.axhline(y=20, color='tab:red', ls='-',alpha=0.4)
+ax1.axhline(y=15, color='tab:purple', ls='-',alpha=0.4)
+
 
 ax.set_ylabel('Power demand [kW]')
-ax1.set_ylabel('T setpoint [°C]')
-ax.set_ylim(0,2)
+ax1.set_ylabel('T in [°C]')
+ax.set_ylim(0,10)
 ax1.set_ylim(0,25)
 ax.legend(labels=['HP pre shift','HP after shift'],loc='upper left',bbox_to_anchor=(0,-0.12))
-ax1.legend(labels=['T set pre shift','T set post shift','T in pre shift', 'T in post shift'],loc = 'upper right',bbox_to_anchor=(1,-0.12))
+labels = ['T in pre shift', 'T in post shift'] # 'T set pre shift','T set post shift',
+ax1.legend(labels=labels,loc = 'upper right',bbox_to_anchor=(1,-0.12))
+
+
+# day ='2015-01-02'
+# ax = Tset_ref.loc[day].plot(legend=True)
+# Tset.loc[day].plot(ax=ax,legend=True)
+# ax.axhline(y=20, color='tab:red', ls='-',alpha=0.4)
+# ax.axhline(y=15, color='tab:purple', ls='-',alpha=0.4)
+# ax.set_ylabel('T setpoint [°C]')
+# ax.set_ylim(0,25)
+# ax.legend(labels=['T set pre shift','T set post shift'], loc='lower right')
 
 
 
+# 1 min timestep
+ts1min=1/60
+Qheat1min,Tin1min = HouseHeating(housetype,QheatHP,Tset,Qintgains,temp,irr,n1min,heatseas_st,heatseas_end,ts1min)
+
+# 1 h timestep
+Tset1h = Tset.resample('60Min').mean()
+# Tset1h = Tset.reindex(index60min,method='nearest')
+Qintgains1h = Qintgains.resample('60Min').mean() # kW
+temp = pd.Series(data=temp[:-1],index=index1min)
+temp1h = temp.resample('60Min').mean()
+# temp1h = temp.reindex(index60min,method='nearest')
+irr = pd.Series(data=irr[:-1],index=index1min)
+irr1h = irr.resample('60Min').mean()
+# irr1h = irr.reindex(index60min,method='nearest')
+n60min = 8760
+ts1h = 1
+Qheat1hour,Tin1hour = HouseHeating(housetype,QheatHP,Tset1h,Qintgains1h,temp1h,irr1h,n60min,heatseas_st,heatseas_end,ts1h)
 
 
-        
+
